71f059e12d580d48fb46cdfa15b6d1fcf1af9be3
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-10-18 12:33:08 +0200

    add migrations

---
+++ back-end/database/migrations/2023_10_18_093507_create_dishes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('ingredientts');
+            $table->tinyInteger('visible')->default(1);
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+};
