cb454b097c74a602efae7fa05fea5a6bf009ba31
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-10-20 11:40:33 +0200

    add user seeder

---
+++ back-end/database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = config("users");
+
+        foreach ($users as $users) {
+            $newUser = new User;
+            $newUser->name = $users["name"];
+            $newUser->lastname = $users["lastname"];
+            $newUser->email = $users["email"];
+            $newUser->password = Hash::make($users["password"]);
+            $newUser->save();
+        }
+    }
+}
