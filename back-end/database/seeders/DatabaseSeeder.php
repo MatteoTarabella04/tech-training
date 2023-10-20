a7546c168d713571d6d0da51a9806402d9dbf941
manolioluigi <luigi.manolio1999@gmail.com>
2023-10-20 12:12:26 +0200

    fix seeder

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             TypologySeeder::class,
-            RestaurantTypologySeeder::class,
             RestaurantSeeder::class,
+            RestaurantTypologySeeder::class,
             DishSeeder::class,
         ]);
     }
