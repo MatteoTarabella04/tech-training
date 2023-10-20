b19767ad36e0f93c156c033adcddab36a8ac9ec6
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-10-20 11:39:09 +0200

    create users elements

---
+++ back-end/config/users.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+   [
+      "name" => "Mario",
+      "lastname" => "Rossi",
+      "email" => "mario.rossi@mail.com",
+      "password" => "rossi123",
+   ],
+   [
+      "name" => "Antonio",
+      "lastname" => "Verdi",
+      "email" => "antonio.verdi@mail.com",
+      "password" => "verdi123",
+   ],
+   [
+      "name" => "Luigi",
+      "lastname" => "Cavalli",
+      "email" => "luigi.cavalli@mail.com",
+      "password" => "cavalli123",
+   ],
+   [
+      "name" => "Vanessa",
+      "lastname" => "Piroi",
+      "email" => "vanessa.piroi@mail.com",
+      "password" => "piroi123",
+   ],
+   [
+      "name" => "Dominique",
+      "lastname" => "Kambula",
+      "email" => "dominique.kambula@mail.com",
+      "password" => "kambula123",
+   ],
+   [
+      "name" => "Chong",
+      "lastname" => "Ming",
+      "email" => "chong.ming@mail.com",
+      "password" => "ming123",
+   ],
+   [
+      "name" => "Vij",
+      "lastname" => "Panjami",
+      "email" => "vij.panjami@mail.com",
+      "password" => "panjami123",
+   ],
+   [
+      "name" => "Sara",
+      "lastname" => "Mulino",
+      "email" => "sara.mulino@mail.com",
+      "password" => "mulino123",
+   ],
+   [
+      "name" => "Daniela",
+      "lastname" => "Severi",
+      "email" => "daniela.severi@mail.com",
+      "password" => "severi123",
+   ],
+   [
+      "name" => "Filippo",
+      "lastname" => "Rughi",
+      "email" => "filippo.rughi@mail.com",
+      "password" => "rughi123",
+   ],
+]
+   ?>
