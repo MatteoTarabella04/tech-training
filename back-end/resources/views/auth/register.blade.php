2a9f689d810e53d65d2dc6b4622db753562e878e
LookasWasTaken~ <piacen.luca@gmail.com>
2023-10-25 12:38:30 +0200

    Add styles

--- back-end/resources/views/auth/register.blade.php
+++ back-end/resources/views/auth/register.blade.php
@@ -77,7 +77,7 @@
 
                         <div class="mb-4 row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn-1 btn-1-blue me-2">
                                     {{ __('Register') }}
                                 </button>
                             </div>
