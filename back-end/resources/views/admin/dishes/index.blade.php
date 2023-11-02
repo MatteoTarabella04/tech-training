c592d2bb9132df6188972819d50e00fe1684fece
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-11-02 12:03:01 +0100

    uncomment destroy

--- back-end/resources/views/admin/dishes/index.blade.php
+++ back-end/resources/views/admin/dishes/index.blade.php
@@ -78,14 +78,15 @@
                                             <div class="modal-content">
                                                 <div class="modal-header">
                                                     <h5 class="modal-title" id="modalTitle-{{ $dish->id }}">
-                                                        Eliminare l'annuncio
+                                                        Eliminare
                                                         {{ $dish->name }}?
                                                     </h5>
                                                 </div>
                                                 <div class="modal-footer">
                                                     <button type="button" class="btn btn-dark"
                                                         data-bs-dismiss="modal">Chiudi</button>
-                                                    <form action="{{-- {{ route('admin.dish.destroy', $dish->slug) }} --}}" method="post">
+                                                    <form action="{{ route('admin.dish.destroy', $dish->id) }}"
+                                                        method="post">
                                                         @csrf
                                                         @method('DELETE')
                                                         <button type="submit" class="btn btn-danger">Elimina</button>
